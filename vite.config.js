6765c2a30bc653090dac656a50d39957383b8c27
Varun Ramanujam <134720150+varunvrcloudtech@users.noreply.github.com>
2025-12-07 17:18:52 -0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5000
+  },
+  optimizeDeps: {
+    include: ['@supabase/supabase-js']
   }
 });
